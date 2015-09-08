03fae94f3bd04768fb85fa5278df6bcb6174a382
zvbsm <tdelgabsm@gmail.com>
2015-09-08 03:13:16 -0700

    Update features
    Added bootstrap modal that contains a form for creating a new post
    Posts include a highlight button so when the user clicks it, the post is
    displayed in a summarized form on the right side to be stored as a
    highlighted post. Later I'd like to make it so when the highlighted post
    is clicked, it will pop up in a modal or new screen with options to
    remove it from the highlighted section

--- app/js/blogControllers.js
+++ app/js/blogControllers.js
@@ -2,14 +2,22 @@
 
 angular.module('blogApp')
 .controller('postsController', function($scope, postsFactory) {
+	$scope.footerText = "You're not signed in." //this value would be updated with "Signed in as (user name)"
+	$scope.highlightedPosts = [];
+
 	postsFactory.success(function(data) {
 	  $scope.posts = data;
 	  console.log($scope.posts);
 	});
 
-	$scope.highlightedPosts = [];
 	$scope.highlightPost = function(post) {
 		console.log(post);
 		$scope.highlightedPosts.push(post);
+	};
+
+	$scope.createPost = function(newPost) {
+		$scope.newPost.date = new Date().getTime();
+		$scope.posts.push(newPost);
+		$scope.newPost = null;
 	}
 });
