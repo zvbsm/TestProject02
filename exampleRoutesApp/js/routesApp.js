dd5958e58eebc7ca7ee3931759ed68e39fc3f44c
Fernando Delgado <tdelgabsm@gmail.com>
2015-09-08 03:25:52 -0700

    Delete routesApp.js

--- exampleRoutesApp/js/routesApp.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('routesApp',['ngRoute'])
-.config(function($routeProvider){
-  $routeProvider
-    .when('/',{templateUrl:'partials/sample.html',controller:'MainPageCtrl'})
-    .when('/pageTwo',{template: '<div>This is page <strong>two</strong></div>'})
-    .otherwise({template: '<div><strong>Theres no page here</strong></div>'})
-});
